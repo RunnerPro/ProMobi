102ab53b2eb762a2107d54cfc54b82a80ead31b6
Sergei St <stro1996@gmail.com>
2017-05-30 19:16:20 +0300

    add ActyvutyLogStyles

--- ActyvityLog.js
+++ ActyvityLog.js
@@ -18,68 +18,12 @@ import {
 
 const { width, height } = Dimensions.get('window')
 
+import stylesActyvityLog from './ActyvityLogSyles'
+
 var List = []
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class ActyvityLog extends Component{
-/*  constructor(){
-
-export default class ActyvityLog extends Component{
-  constructor(){
-
-    super()
-    this.state = {
-      list: '',
-      pressed : false
-    }
-    try {
-      AsyncStorage.getItem('database').then((value) => {
-        console.log(value)
-        this.setState({
-          list: JSON.parse(value)
-        })
-      })
-    } catch(err) {
-      console.log(err)
-    }
-  }
-  parseData(){
-    if ( this.state.list ) {
-      return this.state.list.map((data,i) =>  {
-        return(<View style={styles.dataList} key = {i}>
-          <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-            <Text style={styles.score}>Score</Text>
-            <TouchableOpacity >
-              <Text style={styles.textType}>{data.type}</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row',flexWrap: 'wrap'}}>
-            <Text style={styles.textDistance}>{data.distance + ' ml'}</Text>
-            <Text style={styles.textTime}>Time</Text>
-            <Text style={styles.textSpeed}>Speed</Text>
-          </View>
-        </View>)
-      })
-    }
-  }
-  render(){
-    return(
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={{fontSize:12}}>ActyvityLog</Text>
-          <TouchableOpacity onPress={() => this.props.navigator.replace({id: 2,})}>
-            <Image
-              source = {require('./images/back.png')}
-              style = {styles.imgBack}/>
-          </TouchableOpacity>
-        </View>
-        <ScrollView style={styles.list}>
-                {this.parseData()}
-        </ScrollView>
-      </View>
-    )
-  }
-}*/
 constructor(){
   super()
   this.state = {
@@ -167,12 +111,13 @@ _renderDate(year,mounth){
     if (this.state.mounth != "other" && this.state.mounth != this.state.supprot){
       this.state.supprot = this.state.mounth
         return (
-          <Text style = {styles.padding}>{monthName},{year}</Text>
+          <Text style = {stylesActyvityLog.padding}>{monthName},{year}</Text>
       )
     }
 }
 
 _renderDay(day,date){
+  console.log(day)
   todayDate = new Date
   todayDay = todayDate.getDate()
   todayDayOfWeek = todayDate.getDay()
@@ -202,11 +147,11 @@ _renderDay(day,date){
   }
   if (todayDayOfWeek == day && todayDay == date){
     return(
-      <Text style = {styles.padding}>Today</Text>
+      <Text style = {stylesActyvityLog.headerOfList}>Today</Text>
     )
   }else{
     return(
-      <Text style = {styles.headerOfList}>{nameDay},{date}</Text>
+      <Text style = {stylesActyvityLog.headerOfList}>{nameDay},{date}</Text>
     )
   }
 
@@ -230,35 +175,35 @@ _renderRow(rowData: string, sectionID: number, rowID: number,self){
         {this._renderDay(day, date)}
         <View
           style = {{width : width , height : height /20,flexDirection: 'row'}}>
-          <View style ={styles.forView}>
-            <Text style = {styles.textData}>
+          <View style ={stylesActyvityLog.forView}>
+            <Text style = {stylesActyvityLog.textData}>
               {this._renderPts(rowData.pts)}
             </Text>
-            <Text style = {styles.textPrefix}>
+            <Text style = {stylesActyvityLog.textPrefix}>
               PTS
             </Text>
           </View>
-          <View style ={styles.forView}>
-            <Text style = {styles.textData}>
+          <View style ={stylesActyvityLog.forView}>
+            <Text style = {stylesActyvityLog.textData}>
               {rowData.distance}
             </Text>
-            <Text style = {styles.textPrefix}>
+            <Text style = {stylesActyvityLog.textPrefix}>
               mi
             </Text>
           </View>
-          <View style = {styles.forView}>
-            <Text style = {styles.textData}>
+          <View style = {stylesActyvityLog.forView}>
+            <Text style = {stylesActyvityLog.textData}>
               {this._renderTime(rowData.time)}
             </Text>
-            <Text style = {styles.textPrefix}>
+            <Text style = {stylesActyvityLog.textPrefix}>
               SEC
             </Text>
           </View>
-          <View style = {styles.forView}>
-            <Text style = {styles.textData}>
+          <View style = {stylesActyvityLog.forView}>
+            <Text style = {stylesActyvityLog.textData}>
              {rowData.speed}
             </Text>
-            <Text style = {styles.textPrefix}>
+            <Text style = {stylesActyvityLog.textPrefix}>
               min/mil
             </Text>
           </View>
@@ -272,7 +217,7 @@ _renderListView(){
   var self = this
       return(
         <ListView
-          style={styles.scrollView}
+          style={stylesActyvityLog.scrollView}
           dataSource= {this.state.dataSource}
           renderRow={(data, sectionID,rowID) => this._renderRow(data ,sectionID,rowID , self)}
         />
@@ -344,210 +289,43 @@ componentDidMount() {
 
 render() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-      <View style={styles.viewForButtonAndText}>
+    <View style={stylesActyvityLog.container}>
+      <View style={stylesActyvityLog.header}>
+      <View style={stylesActyvityLog.viewForButtonAndText}>
         <TouchableOpacity onPress ={() => this._handlePressId(2)}>
           <Image
             source = {require('./images/back.png')}
-            style = {styles.imgBack}/>
+            style = {stylesActyvityLog.imgBack}/>
         </TouchableOpacity>
-        <Text style={styles.textLog}>ACTIVITY LOG</Text>
+        <Text style={stylesActyvityLog.textLog}>ACTIVITY LOG</Text>
       </View>
-        <View style = {styles.viewForText}>
-          <Text style = {styles.Activities}>Activities</Text>
-          <Text style = {styles.Points}>Points</Text>
+        <View style = {stylesActyvityLog.viewForText}>
+          <Text style = {stylesActyvityLog.Activities}>Activities</Text>
+          <Text style = {stylesActyvityLog.Points}>Points</Text>
         </View>
-        <View style = {styles.viewForNomber}>
-          <Text style = {styles.nomberActivities}>45</Text>
-          <Text style = {styles.nomberPoints}>576</Text>
+        <View style = {stylesActyvityLog.viewForNomber}>
+          <Text style = {stylesActyvityLog.nomberActivities}>45</Text>
+          <Text style = {stylesActyvityLog.nomberPoints}>576</Text>
         </View>
         <Image
           source = {require('./images/Group 4.png')}
-          style = {styles.imgLine}/>
+          style = {stylesActyvityLog.imgLine}/>
         <Image
           source = {require('./images/Group2.png')}
-          style = {styles.imgPol}/>
+          style = {stylesActyvityLog.imgPol}/>
         <Image
           source = {require('./images/Rectangle 4.png')}
-          style = {styles.imgLevel}>
-            <Text style = {styles.textLevl}>LEVL 2</Text>
+          style = {stylesActyvityLog.imgLevel}>
+            <Text style = {stylesActyvityLog.textLevl}>LEVL 2</Text>
         </Image>
-        <Text style = {styles.ptsToNextLevl}>233 pts. to Level 3</Text>
+        <Text style = {stylesActyvityLog.ptsToNextLevl}>233 pts. to Level 3</Text>
       </View>
       <ProgressViewIOS
-        style = {styles.progres}
+        style = {stylesActyvityLog.progres}
         progressTintColor = '#674DCD'
         progress = {0.5}/>
       {this._renderListView()}
     </View>
   );
-}
-}
-
-const styles = StyleSheet.create({
-  headerOfList : {
-    marginLeft : width/20,
-    color : '#979797',
-    fontFamily : 'Roboto-Regular',
-    marginTop : height*0.01,
-    fontSize : 10
-  },
-  padding : {
-    marginLeft : width/20,
-    color : '#979797',
-    fontFamily : 'Roboto-Regular'
-  },
-  forView : {
-    width :width/6,
-    alignItems : 'center',
-    marginLeft : width/20,
-    flexDirection : 'row'
-  },
-  textPrefix : {
-    fontFamily : 'Roboto-Regular',
-    fontSize : 10,
-    color : '#979797',
-    paddingTop : height*0.007
-  },
-  textData : {
-    fontFamily : 'Roboto-Regular',
-    fontSize : 14
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  header:{
-    width : width,
-    height : height/5,
-    backgroundColor : '#F2F5FA',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-  progres:{
-    width : width,
-    position: 'absolute',
-    top : height/5
-  },
-  scrollView:{
-    width : width,
-    position : 'absolute',
-    top : height/5,
-    marginTop : 5,
-    height : height - height/5
-  },
-  imgPol : {
-    width : width/6,
-    height : height/9,
-    position : 'absolute',
-    top: height*0.06,
-    bottom: 0,
-    left:  width/2 - width/11,
-    right: 0
-  },
-  textLog : {
-    fontFamily: 'Roboto-Regular',
-    backgroundColor : 'transparent',
-    fontSize : 15,
-    marginLeft : width/2.65,
-    marginTop : height*0.009
-  },
-  viewForButtonAndText : {
-    flexDirection : 'row',
-    backgroundColor : 'transparent',
-    width: width,
-    height : height*0.05,
-    marginTop : height*0.025
-  },
-  imgBack : {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom:0 ,
-    position: 'absolute',
-    height:width/10,
-    width:width/9,
-    shadowColor: "#000000",
-    shadowOpacity: 0.2,
-    shadowRadius: 1,
-    shadowOffset: {
-      height: 2,
-      width: 0
-    }
-  },
-  imgLine : {
-    width : width /3,
-    left: width / 3.1,
-    right: 0,
-    top: height/9,
-    bottom:0 ,
-    position: 'absolute',
-  },
-  imgLevel : {
-    width : width/9,
-    height : height /33,
-    position : 'absolute',
-    left : width /2.3,
-    top : height/6.9,
-    bottom : 0,
-    right : 0,
-    borderRadius : 2
-  },
-  textLevl : {
-    backgroundColor : 'transparent',
-    fontFamily : 'Roboto-Bold',
-    color : '#ffffff',
-    fontSize : 10,
-    marginTop : height*0.004,
-    marginHorizontal : width*0.01
-  },
-  ptsToNextLevl : {
-    backgroundColor : 'transparent',
-    marginTop : height/45,
-    fontSize : 10 ,
-    textAlign : 'center',
-    fontFamily : 'Roboto-Regular'
-  },
-  viewForText : {
-    flexDirection : 'row',
-    width: width,
-    height : height*0.04,
-    backgroundColor : 'transparent'
-  },
-  viewForNomber : {
-    flexDirection : 'row',
-    width: width,
-    height : height*0.04,
-    backgroundColor : 'transparent'
-  },
-  Activities : {
-    marginLeft: width/7,
-    fontFamily:'Roboto-Regular',
-    marginTop : height*0.01,
-    color : '#979797'
-  },
-  Points : {
-    marginLeft : width/2.5,
-    fontFamily:'Roboto-Regular',
-    marginTop : height*0.01,
-    color : '#979797'
-  },
-  nomberActivities : {
-    marginLeft:width/6,
-    fontFamily: 'Roboto-Medium',
-    fontSize : 25,
-    marginTop : -height*0.01
-  },
-  nomberPoints : {
-    marginLeft: width/2.2,
-    fontFamily: 'Roboto-Medium',
-    fontSize : 25,
-    marginTop : -height*0.01
   }
-})
+}
